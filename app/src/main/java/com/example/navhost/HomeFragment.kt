75b1b0588a2db8c3f44d1db0a4fdaa324c192d38
macpro <macpro@192.168.2.16>
2023-01-20 20:56:08 +0700

    .

---
+++ app/src/main/java/com/example/navhost/HomeFragment.kt
@@ -0,0 +1,82 @@
+package com.example.navhost
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.navhost.databinding.ActivityMainBinding
+import com.example.navhost.databinding.FragmentDataBinding
+import com.example.navhost.databinding.FragmentHomeBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private lateinit var binding: FragmentHomeBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+
+        val args = getArguments()?.getString("data1")
+        if (args != null) {
+            Log.d("LOG_DATA_FRAGMENT",args )
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.btnLinkHomeFragment.setOnClickListener {
+//            findNavController().navigate(R.id.dataFragment)
+
+            val transaction =  requireActivity().supportFragmentManager.beginTransaction()
+            transaction.addToBackStack(null)
+            transaction.replace(R.id.singleFragment, DataFragment.newInstance("test",""))
+            transaction.commit()
+
+        }
+        return binding.getRoot();
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment HomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/example/navhost/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.navhost
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.navhost.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding!!.root
+
+     /*   val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)*/
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.singleFragment, HomeFragment())
+        transaction.commit()
+        setContentView(view)
+    }
+
+    companion object{
+
+    }
+}
